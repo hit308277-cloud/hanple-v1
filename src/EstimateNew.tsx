ddacf713cfaaf135a55768a876c9886da51118d7
hit308277-cloud <hit308277@gmail.com>
2025-11-27 16:05:42 +0900

    Update EstimateNew.tsx

--- src/EstimateNew.tsx
+++ src/EstimateNew.tsx
@@ -1,10 +1,8 @@
-// src/pages/worktalk/estimate/EstimateNew.tsx
-
 function EstimateNew() {
   return (
     <div style={{ padding: 20 }}>
       <h2>견적서 작성</h2>
-      <p>여기에 견적서 입력 폼이 들어옵니다.</p>
+      <p>여기에 견적서 입력 폼이 들어갑니다.</p>
     </div>
   );
 }
